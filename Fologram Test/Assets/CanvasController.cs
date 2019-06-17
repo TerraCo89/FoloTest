2ccae4a1934210c4d87142ccc502ba46c62aab75
Kris Cernjavic <cernji@live.com>
2019-06-17 16:50:37 +1000

    Changed name of scene to something relevant: Dev Test Scene Step 6 - Create script to control the canvas elements and display mode Step 7 - Button toggles between screen space and world space

---
+++ Fologram Test/Assets/CanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwapCanvasSpace()
+    {
+        switch (canvas.renderMode)
+        {
+            case RenderMode.ScreenSpaceOverlay:
+                canvas.renderMode = RenderMode.WorldSpace;
+                canvas.worldCamera = Camera.main;
+                canvas.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+                canvas.transform.position = new Vector3(-0.5f, 1.5f, 3);
+                break;
+            case RenderMode.WorldSpace:
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                canvas.transform.localScale = new Vector3(1, 1, 1);
+                canvas.transform.position = new Vector3(960, 540, 0);
+                break;
+
+        }
+    }
+}
